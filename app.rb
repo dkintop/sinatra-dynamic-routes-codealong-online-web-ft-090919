7da69488944748cfedffb0b8ba2fbb8481146813
Doug Kintop <dkintop@hotmail.com>
2019-10-21 19:40:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,5 +19,10 @@ class App < Sinatra::Base
     "Goodbye,  #{@user_name}"
   end
   
+  get '/multiply/:number1/:number2' do 
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    @num1 + @num2
+  
   
 end
